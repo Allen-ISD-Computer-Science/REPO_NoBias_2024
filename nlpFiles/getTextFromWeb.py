666d297dd3b004e259deb47a0e04beaa48878457
Omer Erturk <111006230+omerert@users.noreply.github.com>
2023-12-24 19:32:48 -0500

    Added sentiment analysis for each paragraph and for the whole article.

--- nlpFiles/getTextFromWeb.py
+++ nlpFiles/getTextFromWeb.py
@@ -16,17 +16,21 @@ import urllib.request
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from nltk.corpus import wordnet as wn
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+# nltk.download('averaged_perceptron_tagger')
 # from app import input_data
 # print(input_data)
 #TODO Get link from user input
 
 def pasteText(list, link):
+
+    analyzer = SentimentIntensityAnalyzer()
     # Create a new instance of the Safari browser
     driver = webdriver.Safari()
     driver.get(link)
 
 # Wait for a while to allow JavaScript to execute (adjust the time as needed)
-    driver.implicitly_wait(2)
+    driver.implicitly_wait(1)
 
     # Now you can interact with the page using Selenium methods
     # For example, you can retrieve the page source
@@ -35,13 +39,36 @@ def pasteText(list, link):
 
     soupText = BeautifulSoup(pageSource, 'html.parser')
     wantedText = soupText.findAll("p")
+    
+    # Declare variables for average polarity
+    totNegative = 0
+    totNeutral = 0
+    totPositive = 0
+    count = 0
     for para in wantedText:
         sentences = sent_tokenize(para.text.strip())
         sentInPara = ""
         for sent in sentences:
-            sentInPara += " {0}".format(sent)
-        list.append(sentInPara)
-        list.append("----")
+            # tagged = nltk.pos_tag(word_tokenize(sent))
+            if sent != "":
+                tagged = analyzer.polarity_scores(sent)
+                sentInPara += " {0}".format(sent)
+                # Adds to the total polarity based on the key of the dictionary
+                totNegative += tagged["neg"]
+                totNeutral += tagged["neu"]
+                totPositive += tagged["pos"]
+                count += 1
+        
+        if "Reporting by" in sentInPara or "Funding for the" in sentInPara:
+            break
+        if sentInPara != "":
+            list.append(sentInPara)
+            list.append("|")
+            list.append(tagged)
+            list.append("----")
+    list.append(f"{totNegative / count * 100}% Negative, {totNeutral / count * 100}% Neutral, {totPositive / count * 100}% Positive")
+
+
             
            
 
