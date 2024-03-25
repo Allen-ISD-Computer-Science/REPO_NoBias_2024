6c857ea5579c180cce444c5bf44f58302aa6ef60
Omer Erturk <111006230+omerert@users.noreply.github.com>
2024-03-24 21:11:56 -0500

    Changed websites pathway (updated page is default)

--- nobiasapp/urls.py
+++ nobiasapp/urls.py
@@ -4,7 +4,7 @@ from . import views
 from .views import submitLink
 urlpatterns = [
     path("aboutus", views.aboutus, name="aboutus"),
-    path('', submitLink, name='submitLink'),
+    path('submitLink', submitLink, name='submitLink'),
     path("tech", views.tech, name="tech"),
-    path("home", views.home, name="home")
+    path('', views.home, name="home")
 ]  
