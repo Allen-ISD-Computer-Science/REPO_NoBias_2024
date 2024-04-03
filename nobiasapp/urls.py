bf4f7e119f65dc6037ebf96d11cd0e4daf4f644b
Omer Erturk <111006230+omerert@users.noreply.github.com>
2024-04-03 14:35:45 -0500

    Changed 'submitLink' destination to 'home' when submitting a link and removed submitLink

--- nobiasapp/urls.py
+++ nobiasapp/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
 from . import views
-from .views import submitLink
+# from .views import submitLink
 urlpatterns = [
     path("aboutus", views.aboutus, name="aboutus"),
-    path('submitLink', submitLink, name='submitLink'),
+    # path('submitLink', submitLink, name='submitLink'),
     path("tech", views.tech, name="tech"),
     path('home', views.home, name="home")
 ]  
--- nobiasapp/views.py
+++ nobiasapp/views.py
@@ -2,22 +2,22 @@ from django.shortcuts import render
 from nlpFiles.getTextFromWeb import polarityRating, highRatedSent
 from .forms import SubmitLinkForm
 
-def submitLink(request):
-    if request.method == 'POST':
-        form = SubmitLinkForm(request.POST)
-        if form.is_valid():
-            link_object = form.save()  # This saves the link to the database
-            thisVar = link_object.link  # Access the link and store it in thisVar
-            # You can now use thisVar for other Python code
-            paragraph = []
-            polarityRating(paragraph, thisVar)
-            highValuedList = highRatedSent(paragraph)
-            # Redirect or render a success page
-            return render(request, 'successPage.html', {'thisVar': paragraph, "highValuedList": highValuedList})
-    else:
-        form = SubmitLinkForm()
+# def submitLink(request):
+#     if request.method == 'POST':
+#         form = SubmitLinkForm(request.POST)
+#         if form.is_valid():
+#             link_object = form.save()  # This saves the link to the database
+#             thisVar = link_object.link  # Access the link and store it in thisVar
+#             # You can now use thisVar for other Python code
+#             paragraph = []
+#             polarityRating(paragraph, thisVar)
+#             highValuedList = highRatedSent(paragraph)
+#             # Redirect or render a success page
+#             return render(request, 'successPage.html', {'thisVar': paragraph, "highValuedList": highValuedList})
+#     else:
+#         form = SubmitLinkForm()
 
-    return render(request, 'submitLink.html', {'form': form})
+#     return render(request, 'submitLink.html', {'form': form})
 
 def aboutus(request):
     return render(request, 'aboutus.html')
